4ef08815ea210de885daf4346bea9a6881131c8a
Adams Castillo <adams.santos.ap@gmail.com>
2024-04-12 18:39:17 -0300

    criei o container com informaçoes de temperatura

--- src/app/modules/componets/weather-card/weather-card.component.ts
+++ src/app/modules/componets/weather-card/weather-card.component.ts
@@ -1,12 +1,18 @@
 import { weatherDatas } from 'src/app/models/interfaces/WeatherDatas';
 import { Component, Input } from '@angular/core';
+import { faDroplet, faTemperatureHigh, faTemperatureLow, faWind } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-weather-card',
-  templateUrl: './weather-card.component.html',
-  styleUrls: ['./weather-card.component.scss']
+  templateUrl: './weather-card.component.html'
 })
 export class WeatherCardComponent {
+  //dados da previsao do tempo recebido pelo pai
   @Input() weatherDatas!: weatherDatas;
+
+  minTemperatureIcon = faTemperatureLow;
+  maxTemperatureIcon = faTemperatureHigh;
+  humidityIcon = faDroplet;
+  windIcon = faWind;
 
 }
